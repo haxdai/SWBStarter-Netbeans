07e230ee7625762e86982313bc24e70d66bfbaa1
sergio.tellez <sergio.tellez@DADSV05STVB0075.infotec.mx>
2018-01-05 10:28:50 -0600

    method capitalize

--- Components/src/main/java/mx/gob/cultura/portal/response/CountName.java
+++ Components/src/main/java/mx/gob/cultura/portal/response/CountName.java
@@ -35,6 +35,15 @@ public class CountName implements Serializable {
         this.name = name;
     }
 
+    public String getCapitalizeName() {
+        if (null == this.name) return "";
+        this.name = this.name.trim();
+        if (this.name.isEmpty() || this.name.length() < 2) return this.name;
+	this.name = this.name.toLowerCase();
+	this.name = Character.toUpperCase(this.name.charAt(0)) + this.name.substring(1);
+        return this.name;
+    }
+    
     @Override
     public String toString() {
         return "Holder{" + "count=" + count + ", name=" + name + '}';
