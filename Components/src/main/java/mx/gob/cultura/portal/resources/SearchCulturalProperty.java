0660ad7988e3fcb621dcb7ec54d854e776721e1f
Hasdai Pacheco <me@hasdaipacheco.com>
2018-03-11 17:57:01 -0600

    Encoded words for search query. Fixes #47

--- Components/src/main/java/mx/gob/cultura/portal/resources/SearchCulturalProperty.java
+++ Components/src/main/java/mx/gob/cultura/portal/resources/SearchCulturalProperty.java
@@ -6,9 +6,9 @@
 package mx.gob.cultura.portal.resources;
 
 import mx.gob.cultura.portal.request.ListBICRequest;
-import mx.gob.cultura.portal.response.Document;
-import mx.gob.cultura.portal.response.Entry;
+import mx.gob.cultura.portal.response.*;
 import org.semanticwb.Logger;
+import org.semanticwb.SWBPlatform;
 import org.semanticwb.SWBUtils;
 import org.semanticwb.model.WebSite;
 import org.semanticwb.portal.api.SWBParamRequest;
@@ -20,108 +20,104 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import javax.servlet.http.HttpSession;
-import org.semanticwb.SWBPlatform;
-
-import static mx.gob.cultura.portal.resources.PagerAction.NUM_PAGE_LIST;
-import mx.gob.cultura.portal.response.Aggregation;
-import mx.gob.cultura.portal.response.CountName;
-import mx.gob.cultura.portal.response.DateRange;
-import mx.gob.cultura.portal.response.Utils;
 
 /**
  *
  * @author sergio.tellez
  */
 public class SearchCulturalProperty extends PagerAction {
-    
+
     private static final int SEGMENT = 8;
     private static final Logger LOG = SWBUtils.getLogger(SearchCulturalProperty.class);
-    
+
     @Override
     public void doAdmin(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws IOException {
-    	StringBuilder ret = new StringBuilder();
-    	try {
+        StringBuilder ret = new StringBuilder();
+        try {
             if ("add".equals(paramRequest.getAction()) || "edit".equals(paramRequest.getAction())) {
-    		SWBResourceURL url=paramRequest.getRenderUrl();
-    		url.setMode(SWBResourceModes.Mode_ADMIN);
-    		url.setAction("save");
-    		ret.append("<form method=\"POST\" action=\"").append(url.toString()).append("\"> \n");
-    		ret.append("	<div class=swbform> \n");
-    		ret.append("		<table width=\"100%\"  border=\"0\" cellpadding=\"5\" cellspacing=\"0\"> \n");
-    		ret.append("			<tr> \n");
-    		ret.append("				<td class=\"datos\">URL Endpoint: </td> \n");
-    		ret.append("				<td class=\"valores\"> \n");
-    		ret.append("					<input type=\"text\" name=\"endpointURL\" value=\"").append(getResourceBase().getAttribute("endpointURL","").trim()).append("\" size=\"40\">");
-    		ret.append("				</td>");
-    		ret.append("			</tr> \n");
-    		ret.append("			<tr> \n");
-    		ret.append("				<td class=\"datos\">Resultados por página: </td> \n");
-    		ret.append("				<td class=\"valores\"> \n");
-    		ret.append("					<input type=\"text\" name=\"resultsPage\" value=\"").append(getResourceBase().getAttribute("resultsPage","").trim()).append("\" size=\"40\">");
-    		ret.append("				</td>");
-    		ret.append("			</tr> \n");
-    		ret.append("			<tr> \n");
-    		ret.append("				<td colspan=2 align=right> \n");
-    		ret.append("					<br><hr size=1 noshade> \n");
-    		ret.append("					<input type=submit name=btnSave value=\"Enviar\" class=boton> \n");
-    		ret.append("				</td> \n");
-    		ret.append("			</tr> \n"); 
-    		ret.append("		</table> \n");
-    		ret.append("	</div> \n");
-    		ret.append("</form> \n");
+                SWBResourceURL url=paramRequest.getRenderUrl();
+                url.setMode(SWBResourceModes.Mode_ADMIN);
+                url.setAction("save");
+                ret.append("<form method=\"POST\" action=\"").append(url.toString()).append("\"> \n");
+                ret.append("	<div class=swbform> \n");
+                ret.append("		<table width=\"100%\"  border=\"0\" cellpadding=\"5\" cellspacing=\"0\"> \n");
+                ret.append("			<tr> \n");
+                ret.append("				<td class=\"datos\">URL Endpoint: </td> \n");
+                ret.append("				<td class=\"valores\"> \n");
+                ret.append("					<input type=\"text\" name=\"endpointURL\" value=\"").append(getResourceBase().getAttribute("endpointURL","").trim()).append("\" size=\"40\">");
+                ret.append("				</td>");
+                ret.append("			</tr> \n");
+                ret.append("			<tr> \n");
+                ret.append("				<td class=\"datos\">Resultados por página: </td> \n");
+                ret.append("				<td class=\"valores\"> \n");
+                ret.append("					<input type=\"text\" name=\"resultsPage\" value=\"").append(getResourceBase().getAttribute("resultsPage","").trim()).append("\" size=\"40\">");
+                ret.append("				</td>");
+                ret.append("			</tr> \n");
+                ret.append("			<tr> \n");
+                ret.append("				<td colspan=2 align=right> \n");
+                ret.append("					<br><hr size=1 noshade> \n");
+                ret.append("					<input type=submit name=btnSave value=\"Enviar\" class=boton> \n");
+                ret.append("				</td> \n");
+                ret.append("			</tr> \n");
+                ret.append("		</table> \n");
+                ret.append("	</div> \n");
+                ret.append("</form> \n");
             }else if ("save".equals(paramRequest.getAction())) {
-    		getResourceBase().setAttribute("endpointURL", request.getParameter("endpointURL"));
-    		getResourceBase().setAttribute("resultsPage", request.getParameter("resultsPage"));
-    		getResourceBase().updateAttributesToDB();
-   		SWBResourceURL url=paramRequest.getRenderUrl();
+                getResourceBase().setAttribute("endpointURL", request.getParameter("endpointURL"));
+                getResourceBase().setAttribute("resultsPage", request.getParameter("resultsPage"));
+                getResourceBase().updateAttributesToDB();
+                SWBResourceURL url=paramRequest.getRenderUrl();
                 url.setMode(SWBResourceModes.Mode_ADMIN);
                 url.setAction("resume");
                 response.sendRedirect(url.toString());
             }else if ("resume".equals(paramRequest.getAction())) {
-    		SWBResourceURL url=paramRequest.getRenderUrl();
-    		url.setMode(SWBResourceModes.Mode_ADMIN);
-    		url.setAction("edit");
-    		ret.append("<form method=\"POST\" action=\"").append(url.toString()).append("\"> \n");
-    		ret.append("	<div class=swbform> \n");
-    		ret.append("		<table width=\"100%\"  border=\"0\" cellpadding=\"5\" cellspacing=\"0\"> \n");
-    		ret.append("			<tr> \n");
-    		ret.append("				<td class=\"datos\">URL Endpoint: </td> \n");
-    		ret.append("				<td class=\"valores\"> \n").append(getResourceBase().getAttribute("endpointURL","")).append("</td>");
-    		ret.append("			</tr> \n");
-    		ret.append("			<tr> \n");
-    		ret.append("				<td class=\"datos\">Resultados por página: </td> \n");
-    		ret.append("				<td class=\"valores\"> \n").append(getResourceBase().getAttribute("resultsPage","").trim()).append("</td>");
-    		ret.append("			</tr> \n");
-    		ret.append("			<tr> \n");
-    		ret.append("				<td colspan=2 align=right> \n");
-    		ret.append("					<br><hr size=1 noshade> \n");
-    		ret.append("					<input type=submit name=btnSave value=\"Regresar\" class=boton> \n");
-    		ret.append("				</td> \n");
-    		ret.append("			</tr> \n"); 
-    		ret.append("		</table> \n");
-    		ret.append("	</div> \n");
-    		ret.append("</form> \n");
+                SWBResourceURL url=paramRequest.getRenderUrl();
+                url.setMode(SWBResourceModes.Mode_ADMIN);
+                url.setAction("edit");
+                ret.append("<form method=\"POST\" action=\"").append(url.toString()).append("\"> \n");
+                ret.append("	<div class=swbform> \n");
+                ret.append("		<table width=\"100%\"  border=\"0\" cellpadding=\"5\" cellspacing=\"0\"> \n");
+                ret.append("			<tr> \n");
+                ret.append("				<td class=\"datos\">URL Endpoint: </td> \n");
+                ret.append("				<td class=\"valores\"> \n").append(getResourceBase().getAttribute("endpointURL","")).append("</td>");
+                ret.append("			</tr> \n");
+                ret.append("			<tr> \n");
+                ret.append("				<td class=\"datos\">Resultados por página: </td> \n");
+                ret.append("				<td class=\"valores\"> \n").append(getResourceBase().getAttribute("resultsPage","").trim()).append("</td>");
+                ret.append("			</tr> \n");
+                ret.append("			<tr> \n");
+                ret.append("				<td colspan=2 align=right> \n");
+                ret.append("					<br><hr size=1 noshade> \n");
+                ret.append("					<input type=submit name=btnSave value=\"Regresar\" class=boton> \n");
+                ret.append("				</td> \n");
+                ret.append("			</tr> \n");
+                ret.append("		</table> \n");
+                ret.append("	</div> \n");
+                ret.append("</form> \n");
             }
-    	}catch (Exception e) { LOG.info(e.getMessage());}
-    	response.getWriter().print(ret.toString());
+        }catch (Exception e) { LOG.info(e.getMessage());}
+        response.getWriter().print(ret.toString());
     }
-    
+
     @Override
     public void doView(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, java.io.IOException {
-    	request.setCharacterEncoding("UTF-8");
-        Document document = null;
+        //response.setCharacterEncoding("UTF-8");
         List<Entry> publicationList = new ArrayList<>();
-    	String url = "/swbadmin/jsp/rnc/documents.jsp";
-    	RequestDispatcher rd = request.getRequestDispatcher(url);
+        String url = "/swbadmin/jsp/rnc/documents.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(url);
+        String q = request.getParameter("word");
 
-        if (null != request.getParameter("word") && !request.getParameter("word").isEmpty()) {
-            document = getReference(request, paramRequest.getWebPage().getWebSite());
+        if (null != q && !q.isEmpty()) {
+            Document document = getReference(request, paramRequest.getWebPage().getWebSite());
             if (null != document) {
                 publicationList = document.getRecords();
                 request.setAttribute("aggs", getAggregation(document.getAggs()));
@@ -129,7 +125,7 @@ public class SearchCulturalProperty extends PagerAction {
                 request.getSession().setAttribute(FULL_LIST, document.getRecords());
                 request.getSession().setAttribute("NUM_RECORDS_TOTAL", document.getTotal());
             }
-            request.setAttribute("word", request.getParameter("word"));
+            request.setAttribute("word", q);
             init(request, response, paramRequest);
         }
 
@@ -141,14 +137,14 @@ public class SearchCulturalProperty extends PagerAction {
             LOG.error(se);
         }
     }
-    
+
     @Override
     public void doSort(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws SWBResourceException, java.io.IOException {
         Document document = null;
         List<Entry> publicationList = new ArrayList<>();
         try {
             if (null != request.getParameter("word") && !request.getParameter("word").isEmpty()) {
-    		document = getReference(request, paramRequest.getWebPage().getWebSite());
+                document = getReference(request, paramRequest.getWebPage().getWebSite());
                 if (null != document) {
                     publicationList = document.getRecords();
                     request.getSession().setAttribute(FULL_LIST, document.getRecords());
@@ -156,15 +152,15 @@ public class SearchCulturalProperty extends PagerAction {
                 request.setAttribute("f", request.getParameter("sort"));
                 request.setAttribute("word", request.getParameter("word"));
                 init(request, response, paramRequest);
-    	    }
+            }
             request.setAttribute("references", publicationList);
-	    request.setAttribute("paramRequest", paramRequest);
-	}catch (Exception se) {
+            request.setAttribute("paramRequest", paramRequest);
+        }catch (Exception se) {
             LOG.error(se);
-	}
+        }
         super.doSort(request, response, paramRequest);
     }
-    
+
     @Override
     public void doPage(HttpServletRequest request, HttpServletResponse response, SWBParamRequest paramRequest) throws java.io.IOException {
         int pagenum = 0;
@@ -197,7 +193,7 @@ public class SearchCulturalProperty extends PagerAction {
             LOG.info(se.getMessage());
         }
     }
-    
+
     private Document getReference(HttpServletRequest request, WebSite site) {
         Document document = null;
         String words = request.getParameter("word");
@@ -207,11 +203,16 @@ public class SearchCulturalProperty extends PagerAction {
         if (null == baseUri || baseUri.isEmpty()) {
             baseUri = SWBPlatform.getEnv("rnc/endpointURL",
                     getResourceBase().getAttribute("endpointURL",
-            "http://localhost:8080")).trim();
+                            "http://localhost:8080")).trim();
         }
 
-    	String uri = baseUri + "/api/v1/search?q=";
-    	uri += getParamSearch(words);
+        String uri = baseUri + "/api/v1/search?q=";
+        try {
+            uri += URLEncoder.encode(getParamSearch(words), StandardCharsets.UTF_8.name());
+        } catch (UnsupportedEncodingException uex) {
+            LOG.error(uex);
+        }
+
         uri += getRange(request);
         if (null != request.getParameter("sort")) {
             if (request.getParameter("sort").equalsIgnoreCase("datedes")) uri += "&sort=-datecreated.value";
@@ -219,7 +220,7 @@ public class SearchCulturalProperty extends PagerAction {
             if (request.getParameter("sort").equalsIgnoreCase("statdes")) uri += "&sort=-resourcestats.views";
             if (request.getParameter("sort").equalsIgnoreCase("statasc")) uri += "&sort=resourcestats.views";
         }
-	ListBICRequest req = new ListBICRequest(uri);
+        ListBICRequest req = new ListBICRequest(uri);
         try {
             document = req.makeRequest();
         }catch (Exception se) {
@@ -227,7 +228,7 @@ public class SearchCulturalProperty extends PagerAction {
         }
         return document;
     }
-    
+
     private List<String> getCreators(List<Entry> records) {
         List<String> creators = new ArrayList<>();
         if (null != records) {
@@ -241,12 +242,12 @@ public class SearchCulturalProperty extends PagerAction {
         }
         return creators;
     }
-    
+
     public static String getCapitalizeName(String name) {
         if (null == name) return "";
         name = name.trim();
         if (name.isEmpty() || name.length() < 2) return name;
-	name = name.toLowerCase();
+        name = name.toLowerCase();
         if (name.contains(" ")) {
             String[] words = name.split(" ");
             StringBuilder capitalize = new StringBuilder();
@@ -266,7 +267,7 @@ public class SearchCulturalProperty extends PagerAction {
             name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
         return name;
     }
-    
+
     private Aggregation getAggregation(List<Aggregation> aggs) {
         DateRange interval = new DateRange();
         Calendar cal = Calendar.getInstance();
@@ -292,61 +293,61 @@ public class SearchCulturalProperty extends PagerAction {
         }
         return aggregation;
     }
-    
+
     private String getRange(HttpServletRequest request) {
         int s = 0;
-    	StringBuilder range = new StringBuilder("&from=");
-    	String start = request.getParameter("p");
+        StringBuilder range = new StringBuilder("&from=");
+        String start = request.getParameter("p");
         if (null != start) {
-            try { 
+            try {
                 s = Integer.parseInt(start);
                 if (s > 1) s = (s-1)*SEGMENT;
             }catch (NumberFormatException e) { }
         }
         range.append(String.valueOf(s));
-    	range.append("&size=").append(SEGMENT);
-    	return range.toString();
+        range.append("&size=").append(SEGMENT);
+        return range.toString();
     }
-    
+
     /**private List<Entry> getRange(int range, List<Entry> records) {
-        List<Entry> publicationListTmp = new ArrayList<>();
-        if (null != records && records.size() > SEGMENT) {
-            int count = 0;
-            for (int i = range; i < records.size(); i++) {
-                publicationListTmp.add(records.get(i));
-                count++;
-                if (count == SEGMENT) break;
-            }
-            records = publicationListTmp;
-        }
-        return records;
-    }**/
-    
+     List<Entry> publicationListTmp = new ArrayList<>();
+     if (null != records && records.size() > SEGMENT) {
+     int count = 0;
+     for (int i = range; i < records.size(); i++) {
+     publicationListTmp.add(records.get(i));
+     count++;
+     if (count == SEGMENT) break;
+     }
+     records = publicationListTmp;
+     }
+     return records;
+     }**/
+
     private String getParamSearch(String words) {
-    	StringBuilder parameters = new StringBuilder();
-    	String fix = words.replaceAll(",", " ").replaceAll(" ,", " ").replaceAll(", ", " ");
-    	String[] search = fix.split(" ");
-    	if (search.length > 0) {
-	    for (int i=0; i<search.length; i++) {
+        StringBuilder parameters = new StringBuilder();
+        String fix = words.replaceAll(",", " ").replaceAll(" ,", " ").replaceAll(", ", " ");
+        String[] search = fix.split(" ");
+        if (search.length > 0) {
+            for (int i=0; i<search.length; i++) {
                 parameters.append("%2B");
-	    	String param  = search[i].trim();
-	    	parameters.append(param);
-	    }
-	    if (parameters.length() > 1)
-	    	parameters.delete(0, 3);
-	    return parameters.toString();
-    	}else 
+                String param  = search[i].trim();
+                parameters.append(param);
+            }
+            if (parameters.length() > 1)
+                parameters.delete(0, 3);
+            return parameters.toString();
+        }else
             return words;
     }
-    
+
     /**private int getStart(HttpServletRequest request) {
-    	int s = 1;
-    	String start = request.getParameter("s");
-    	try { 
-            if (null != start)
-                s = Integer.parseInt(start); 
-        }catch (NumberFormatException e) { }
-        if (s > 0) s = s*SEGMENT-1;
-    	return s;
-    }**/
+     int s = 1;
+     String start = request.getParameter("s");
+     try {
+     if (null != start)
+     s = Integer.parseInt(start);
+     }catch (NumberFormatException e) { }
+     if (s > 0) s = s*SEGMENT-1;
+     return s;
+     }**/
 }
